d10ef889394d9ffd6645db13004c338d1364970e
Lenilson Junior <lenilsonjrpro@gmail.com>
2023-12-04 19:21:13 -0300

    commit inicial

---
+++ JS/scripts.js
@@ -0,0 +1,30 @@
+const minutesEl = document.querySelector('#minutes');
+const secondsEl = document.querySelector('#seconds');
+const milesecondsEl = document.querySelector('#milliseconds');
+const startBtn = document.querySelector('#startBtn');
+const pauseBtn = document.querySelector('#pauseBtn');
+const resumeBtn = document.querySelector('#resumeBtn');
+const resetbtn = document.querySelector('#resetBtn');
+
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let isPaused = false;
+
+startBtn.addEventListener("click", startTimer)
+
+function startTimer(){
+    interval = setInterval(() =>{
+        if(!isPaused){
+            milliseconds += 10
+            if(milliseconds === 1000){
+                seconds++;
+                milesecondsEl = 0;
+            }
+            if(seconds === 60){
+                minutes++;
+                seconds = 0
+            }
+        }
+    }, 10)
+}
